524e9f04da197eb54f1da2b1d321f45179af54ba
Sean070103 <mendozaseanmichaelandrewb2345@gmail.com>
2025-01-30 16:07:31 -0500

    tokens issue

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,26 +4,18 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: {
     compilers: [
-      {
-        version: "0.8.4",
-      },
-      {
-        version: "0.8.20",
-      },
-      {
-        version: "0.8.22",
-      },
-      {
-        version: "0.8.28",
-      },
+      { version: "0.8.4" },
+      { version: "0.8.20" },
+      { version: "0.8.22" },
+      { version: "0.8.28" },
     ],
   },
   networks: {
     sepolia: {
       url: "https://sepolia.infura.io/v3/8a5ff716e38f4b5bb185142c35634b59",
       accounts: [
-        '59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
+        "f902046a6912b153bd372c50f02217d6dec3c4306b1461f667114a97615cd966",
       ],
-    },  
+    },
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,39 @@
+const { ethers } = require("hardhat");
+const fs = require("fs");
+
 async function main() {
   const [deployer] = await ethers.getSigners();
   console.log("Deploying contracts with the account:", deployer.address);
 
+  // Deploy MyToken contract
   const MyToken = await ethers.getContractFactory("MyToken");
-  const myToken = await MyToken.deploy();
+  const myToken = await MyToken.deploy(
+    "MyTokenName",
+    "MTK",
+    18,
+    ethers.utils.parseEther("1000000")
+  );
   await myToken.deployed();
   console.log("MyToken deployed to:", myToken.address);
 
+  // Deploy MyNFT contract
   const MyNFT = await ethers.getContractFactory("MyNFT");
-  const myNFT = await MyNFT.deploy();
+  const myNFT = await MyNFT.deploy("MyNFTCollection", "MNFT");
   await myNFT.deployed();
   console.log("MyNFT deployed to:", myNFT.address);
+
+  // Save contract addresses for reference
+  fs.writeFileSync(
+    "deployedAddresses.json",
+    JSON.stringify({ myToken: myToken.address, myNFT: myNFT.address }, null, 2)
+  );
+
+  console.log("Deployment addresses saved.");
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
+    console.error("Error deploying:", error);
     process.exit(1);
   });
